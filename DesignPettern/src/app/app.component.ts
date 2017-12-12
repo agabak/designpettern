ce0f63892397b3a9fdb75c62d86b3c5d8f5b71e3
Kabunga <Agaba.Kabunga@nov.com>
2017-12-12 14:05:48 -0600

    initial check in

---
+++ DesignPettern/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+    // tslint:disable-next-line:component-selector
+    selector: 'my-app',
+    styles: [`
+      .app {
+        width: 1000px;
+        margin: auto;
+      }
+      .main {
+        background-color:white;
+      }
+    `],
+    template: `
+      <div class="app">
+        <wb-nav-bar></wb-nav-bar>
+        <div class="main">
+          <router-outlet></router-outlet>
+        </div>
+      </div>
+    `,
+})
+export class AppComponent  {  }
